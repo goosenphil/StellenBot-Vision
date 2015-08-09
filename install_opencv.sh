4cca380871935e47a113f391e0beab8017849518
philipgoosen <philip@manmakemachine.com>
2015-08-09 14:51:24 +0200

    Added ubuntu/debian dependencies in comments, uncomment line to automatically installl them. This commit message is being repeated to test if slack integration is working, also I have lowered the amount of threads used during compilation

--- install_opencv.sh
+++ install_opencv.sh
@@ -14,7 +14,7 @@ cd opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=~/projects/OpenCV3/opencv_contrib/modules -D PYTHON_INCLUDE_DIR=/usr/include/python2.7 -D PYTHON_INCLUDE_DIR2=/usr/include/x86_64-linux-gnu/python2.7 -D PYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so ..
-make -j4 #You might want to tweak this variable to a lower amount depending on your CPU
+make -j2 #You might want to tweak this variable to a lower amount depending on your CPU
 sudo make install
 
 sudo echo '/usr/local/lib' > /etc/ld.so.conf.d/opencv.conf
