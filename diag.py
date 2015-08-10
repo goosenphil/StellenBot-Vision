4cb621f485a70075075c38301c821d457845bc47
philipgoosen <philip@manmakemachine.com>
2015-08-10 13:13:17 +0200

    Added diagnostic script to test if OpenCV is working. Also improved install script to automatically install packages for debian/ubuntu users. Added test image.

---
+++ diag.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+
+# This is a diagnostic script to make sure that OpenCV is working on linux
+
+try:
+    import cv2
+except ImportError:
+    print "You must have OpenCV installed"
+
+try:
+    import numpy as np
+    print "[+] Numpy installed!"
+except ImportError:
+    print "[-]You must have numpy installed"
+
+def check_install():    
+    print "Checking if latest build of OpenCV is installed..."
+    if (cv2.__version__ == "3.0.0-dev"):
+        print "[+]Good, you're running the latest source build"
+    else:
+        print "[-]NOPE! You're running release: " + cv2.__version__
+
+def test_image():
+    print "Now displaying, press any key to exit"
+    image = cv2.imread('flag.png')
+    cv2.imshow("Image", image)
+    print "Press any key to exit..."
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    print "Debug mode on, look in the comments on how to fix the problems."
+    check_install()
+    test_image()
+
+# Notes:
+# To fix the error: (python:20917): Gtk-WARNING **: Unable to locate theme engine in module_path: "pixmap", 
+# install the gtk2-engines-pixbuf package
+# installing numpy: sudo apt-get install python-pip python-dev && sudo pip install numpy
+# For installing openCV, use script the script from the repo
--- install_opencv.sh
+++ install_opencv.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
 # This install script is meant for linux
-# To install ubuntu/debian dependencies, uncomment the next line: 
-#sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
-echo " This script needs QT installed (0r remove -D WITH_QT=ON from build options)"
-read -n1 -p "This will clone and build the latest OpenCV in the current directory, press any key to continue..."
+echo "================================================================================================================="
+echo "This script will install the latest OpenCV from source with Python and C++ bindings."
+echo "This script needs QT installed (0r remove -D WITH_QT=ON from build options)"
+echo "Please note that OpenCV is a very large download, github access is free without inetkey so please close it."
+echo "You may also require to download a few packages, please set your repository to ftp.sun.ac.za to download them for free"
+echo "This can easily be selected from the ubuntu software centre"
+read -n1 -p "This will clone and build the latest OpenCV in the current directory, press any key to continue... (Press Ctrl+C to exit)"
 
+if [ -x "$(command -v apt-get)" ]; then
+    echo "Looks like you are running a debian based distro like Ubuntu, now installing packages"
+    sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev gtk2-engines-pixbuf
+else
+    echo "Not running a debian based distro, please install the packages"
+fi
 
+echo "Now downloading OpenCV from github..."
 git clone https://github.com/Itseez/opencv.git
 git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv
 mkdir build
 cd build
+
+echo "Done, now compiling. This might take very long..."
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=~/projects/OpenCV3/opencv_contrib/modules -D PYTHON_INCLUDE_DIR=/usr/include/python2.7 -D PYTHON_INCLUDE_DIR2=/usr/include/x86_64-linux-gnu/python2.7 -D PYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so ..
-make -j2 #You might want to tweak this variable to a lower amount depending on your CPU
+make -j2 #You might want to tweak this variable to a lower/higher amount depending on your CPU.
 sudo make install
 
 sudo echo '/usr/local/lib' > /etc/ld.so.conf.d/opencv.conf
