ca21fe1962b912c372c79e20df94ad961b0c825b
philipgoosen <philip@manmakemachine.com>
2015-09-11 14:55:06 +0200

    Added example for serial control

---
+++ serialExample.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+import serial
+
+s = serial.Serial('/dev/robot')
+speed = 0
+#s.open()
+s.write( chr(0)  + chr(0) + chr(speed) )
+s.write( chr(1)  + chr(1) + chr(speed) )
+s.close()
