b9149cd0aaaa04e3b0941cad0a4dfe0497bad294
philipgoosen <philip@manmakemachine.com>
2015-09-24 17:00:06 +0200

    Created cropping offset

--- tools/lineTrack.py
+++ tools/lineTrack.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+from random import randint
 
 def nothing(x):
     pass
@@ -9,9 +10,9 @@ def nothing(x):
 def centroid(moments):
     return (int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00']))
 
-def crop(x1, y1, x2, y2):
-    tempFrame = np.copy(frame)
-    cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,255),5)
+def crop(inframe, x1, y1, x2, y2):
+    tempFrame = np.copy(inframe) # Prevents drawing onto frame if cropping from same frame
+    cv2.rectangle(frame,(x1,y1),(x2,y2), ( x1-y2, y1-y2, x2-y2 ) ,5)
     return tempFrame[y1:y2, x1:x2]
 
 cap = cv2.VideoCapture(1)
@@ -27,11 +28,14 @@ midw = width/2
 midh = height/2
 print midw, midh
 
+cutSize = 50
+offset = 100
+
 while(1):
 
     # Take each frame
     _, frame = cap.read()
-    cv2.imshow('crop', crop(midw-100,0, midw+100,height) )
+    cv2.imshow('crop', crop(frame, midw-100,0, midw+100,height) )
     cv2.circle(frame,(midw,midh), 5, (0,255,0), 5)
     cv2.imshow('frame',frame)
     # cv2.imshow('crop', crop(100,200,300,400, frame))
@@ -44,6 +48,10 @@ while(1):
     mask2 = np.copy(mask)
     _, contours, hierarchy = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(frame, contours, -1, (255,0,0), 3)
+
+    sets = crop(mask, midw-100,offset, midw+100,offset+cutSize), crop(mask, midw-100,height-cutSize-offset, midw+100,height-offset)
+    cv2.imshow('top', sets[0])
+    cv2.imshow('bot', sets[1])
 
     # for a in contours:
         # if 1000 > cv2.contourArea(a) > 5:
@@ -67,6 +75,6 @@ while(1):
         break
     if k == 32: # Check for spacebar (To reset sliders)
         # resetSliders()
-        print frame.shape[0], frame.shape[1]
+        print srandint(0,255)
 
 cv2.destroyAllWindows()
