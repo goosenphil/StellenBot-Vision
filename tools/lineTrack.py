5c867392b6916f31933569031bf35dc4b24f6bfa
philipgoosen <philip@manmakemachine.com>
2015-09-24 17:50:43 +0200

    Fixed unreliable centroid tracking

--- tools/lineTrack.py
+++ tools/lineTrack.py
@@ -2,7 +2,6 @@
 
 import cv2
 import numpy as np
-from random import randint
 
 def nothing(x):
     pass
@@ -10,20 +9,22 @@ def nothing(x):
 def centroid(moments):
     try:
         return (int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00']))
+        # return 0,0
     except:
         return 0,0
         # pass
 
-def crop(inframe, x1, y1, x2, y2):
-    tempFrame = np.copy(inframe) # Prevents drawing onto frame if cropping from same frame
-    cv2.rectangle(frame,(x1,y1),(x2,y2), ( x1-y2, y1-y2, x2-y2 )  ,5) #The part that says ( x1-y2, y1-y2, x2-y2 ) is to create constant but differing colours for each rectangle based on positions
-    return tempFrame[y1:y2, x1:x2]
+def crop(inFrame, x1, y1, x2, y2):
+    # tempFrame = np.copy() # Prevents drawing onto frame if cropping from same frame
+    cv2.rectangle(drawFrame,(x1,y1),(x2,y2), ( x1-y2, y1-y2, x2-y2 )  ,5) #The part that says ( x1-y2, y1-y2, x2-y2 ) is to create constant but differing colours for each rectangle based on positions
+    return inFrame[y1:y2, x1:x2]
 
 cap = cv2.VideoCapture(1)
 cv2.namedWindow('mask')
-cv2.createTrackbar('thresh','mask',0,255,nothing)
-cv2.createTrackbar('cutSize','frame',0,255,nothing)
-cv2.createTrackbar('offset','frame',0,255,nothing)
+cv2.createTrackbar('thresh','mask',55,255,nothing)
+# cv2.createTrackbar('cutSize','frame',0,255,nothing)
+# cv2.createTrackbar('offset','frame',0,255,nothing)
+# cv2.setTrackbarPos('thesh', 'mask', 55)
 
 _, test = cap.read()
 # print test.shape
@@ -34,15 +35,17 @@ midw = width/2
 midh = height/2
 # print midw, midh
 
-cutSize = 50
+cutSize = 30
 offset = 400
+wait = 5
 
 while(1):
 
     _, frame = cap.read()
+    drawFrame = np.copy(frame)
     cv2.imshow('crop', crop(frame, midw-100,0, midw+100,height) )
-    cv2.circle(frame,(midw,midh), 5, (0,255,0), 5)
-    cv2.imshow('frame',frame)
+    # cv2.circle(frame,(midw,midh), 5, (0,255,0), 5)
+    # cv2.imshow('frame',frame)
     # cv2.imshow('crop', crop(100,200,300,400, frame))
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -51,8 +54,8 @@ while(1):
     cv2.imshow('mask',mask)
 
     mask2 = np.copy(mask)
-    _, contours, hierarchy = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame, contours, -1, (255,0,0), 3)
+    # _, contours, hierarchy = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(frame, contours, -1, (255,0,0), 3)
 
     cuts = crop(mask, midw-100,offset, midw+100,offset+cutSize), crop(mask, midw-100,height-cutSize-offset, midw+100,height-offset)
 
@@ -62,25 +65,37 @@ while(1):
     corners = [(midw-100,offset), (midw-100,height-cutSize-offset)] # The top left corners of the rectangles
     # print corner
 
-    cv2.circle(frame,corners[0], 5, (0,255,0), 5)
-    cv2.circle(frame,corners[1], 5, (0,255,0), 5)
+    # cv2.circle(frame,corners[0], 5, (0,255,0), 5)
+    # cv2.circle(frame,corners[1], 5, (0,255,0), 5)
+    # cv2.circle(frame,tuple(np.add(corners[0],(197,24))), 5, (0,255,0), 5)
     # cv2.imshow('top', cuts[0])
     # cv2.imshow('bot', cuts[1])
 
+    #It mighe be easier if I place masks on top of black frame of same resolution as original to not have to keep track of offsets
+    #ToDo, try to remove 'jumping', point cannot traverse amount of units per amount of frames
+    prev = (0,0)
+    z = crop(frame, midw-100,offset, midw+100,offset+cutSize)
+    cv2.imshow('z',z)
     for a,corn in zip(cuts,corners):
         _, contours, hierarchy = cv2.findContours(np.copy(a), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for b in contours:
-            if cv2.contourArea(b) > 0:
+            if cv2.contourArea(b) > 10:
                 cord = centroid(cv2.moments(contours[0]))
                 # if cord != (0,0)
                 # cv2.circle(frame,(cord[0],cord[1]), 5, (255,0,255), 5)
-                cv2.circle(frame,(cord[0]+corn[0],cord[1]+corn[1]), 5, (255,0,255), 5)
+                cv2.circle(drawFrame,(cord[0]+corn[0],cord[1]+corn[1]), 5, (0,0,255), 5)
                 # cv2.circle(frame,(cord[0]+corn1[1],cord[1]+corn1[1]), 5, (255,0,255), 5)
-                print "[",cord[0], cord[1], "]"
-                cv2.drawContours(a, contours, -1, (255,0,0), 3)
+                # print "[",cord[0], cord[1], "]"
 
-    # cv2.imshow('top', cuts[0])
-    # cv2.imshow('bot', cuts[1])
+                if cord is not prev:
+                    print cord
+                    prev = cord
+                cv2.drawContours(a, contours, -1, (0,0,0), 1)
+                # print contours
+
+    # cv2.line(test, (lin[0],lin[1]),(lin[2],lin[3]), (255,0,0), 5)
+    cv2.imshow('top', cuts[0])
+    cv2.imshow('bot', cuts[1])
     # for a in contours:
         # if 1000 > cv2.contourArea(a) > 5:
             # print cv2.contourArea(a)
@@ -94,15 +109,21 @@ while(1):
     # filter = cv2.dilate(filter, kd, iterations = id)
     # cv2.imshow('filter', filter)
 
-    cv2.imshow('Final frame',frame)
+    cv2.imshow('Source image, make sure I am unmodified!', frame) # Make sure this frame is unmodified at the end, to avoid conflicts in calculations
+    cv2.imshow('Draw frame',drawFrame)
 
     # cv2.imshow('crop', crop(100,200,300,400, frame))
 
-    k = cv2.waitKey(5) & 0xFF
+    k = cv2.waitKey(wait) & 0xFF
     if k == 27: #Checks if escape is pressed
         break
     if k == 32: # Check for spacebar (To reset sliders)
         # resetSliders()
-        print srandint(0,255)
+        # wait = 1000
+        if wait is 1000:
+            wait = 5
+        if wait is 5:
+            wait = 1000
+
 
 cv2.destroyAllWindows()
