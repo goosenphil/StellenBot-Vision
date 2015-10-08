26bab433ad2873e1b530f27881a7fff37b4d9b1a
philipgoosen <philip@manmakemachine.com>
2015-10-09 00:41:02 +0200

    Fixed more logical errors. Defined states.

--- tools/blockRush.py
+++ tools/blockRush.py
@@ -12,6 +12,12 @@ s = serial.Serial('/dev/ttyACM0', 115200)
 
 cap = cv2.VideoCapture(1)
 
+# lastSend =
+def send(byte1, byte2, byte3):
+    s.write( chr(101) + chr(byte1)  + chr(byte2) + chr(byte3) )
+    # print "SERIAL DISABLED!", str(byte1), str(byte2), str(byte3)
+    pass
+
 def nothing(x):
     pass
 
@@ -26,13 +32,16 @@ def checkMs(diff):
 
     return False
 
-def resetSliders(): #Sets sliders to their default positions
-    cv2.setTrackbarPos('Bl', 'mask', 0)
-    cv2.setTrackbarPos('Gl', 'mask', 0)
-    cv2.setTrackbarPos('Rl', 'mask', 0)
-    cv2.setTrackbarPos('Bu', 'mask', 255)
-    cv2.setTrackbarPos('Gu', 'mask', 255)
-    cv2.setTrackbarPos('Ru', 'mask', 255)
+stopTick = 0.0
+def checkMsStop(diff):
+    newTime = time.time() * 1000
+    global stopTick
+
+    if (newTime-stopTick > diff):
+        stopTick = newTime
+        return True
+
+    return False
 
 def drawFeatures(cx, cy, sides, conA):
     cv2.circle(drawFrame,(cx,cy), 5, (255,0,0), -1)
@@ -48,42 +57,48 @@ def doRobot(angle):
         speed = 75
     if speed < 70:
         speed = 75
-    print "[",str(speed),"]"
+    # print "[",str(speed),"]"
     if (tol > abs(angle) ): # Attempt to move forward when on line
         cv2.putText(drawFrame,"|"+str(speed)+"|",(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
         send(0, 1, 80)
         send(1, 1, 80)
-        print "______________"
     elif ( angle < -tol): # Attempt to move right towards line
-        print "<<<<<<<<<<<<<<"
         cv2.putText(drawFrame,"<"+str(speed),(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
         send(1,1,speed)
         send(1,1,speed-10)
     elif( angle > tol): # Attempt to move left towards line
         cv2.putText(drawFrame,(str(speed)+">"),(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
-        print ">>>>>>>>>>>>>>"
         send(0,1,speed)
         send(1,1,speed-10)
 
 def stopRobot():
-    send(1,1,0)
-    send(0,1,0)
+    if checkMsStop(100):
+        send(1,1,0)
+        send(0,1,0)
+
+def rotateRobot():
+    send(1, 0, 75)
+    send(1, 1, 70)
 
 def centroid(contour):
     mo = cv2.moments(contour)
-    cx = int(mo['m10']/mo['m00'])
-    cy = int(mo['m01']/mo['m00'])
-    return (cx, cy)
+    try:
+        cx = int(mo['m10']/mo['m00'])
+        cy = int(mo['m01']/mo['m00'])
+        return (cx, cy)
+    except:
+        return 0, 0
 
-def send(byte1, byte2, byte3):
-    s.write( chr(101) + chr(byte1)  + chr(byte2) + chr(byte3) )
+
+def seekBlocks():
+    rotateRobot()
 
 def trackBlock(mask, drawFrame):
     _, contours, hierarchy = cv2.findContours(np.copy(mask), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:5] # Reduces contours to only top 5 based on area.
-    blockVisible = True
-
+    global blockVisible
+    blockVisible = False
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:1] # Reduces contours to only top 5 based on area.
+    cx, cy, conA = 0, 0, 0
 
     for c in contours:
         conA = cv2.contourArea(c)
@@ -93,30 +108,14 @@ def trackBlock(mask, drawFrame):
             approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
             if 4 <= len(approx) < 10: # Look for object within specific range of sides
-                blockVisible = False
+                blockVisible = True
+                centre = centroid(c)
+                cx = centre[0]
+                cy = centre[1]
+                drawFeatures(cx, cy, len(approx), conA)
+                cv2.drawContours(drawFrame, [approx], -1, (0, 200, 50), 3)
 
-                try:
-                    centre = centroid(c)
-                    cx = centre[0]
-                    cy = centre[1]
-
-                    if checkMs(10):
-                        if robotEnabled:
-                            drawFeatures(cx, cy, len(approx), conA)
-                            cv2.drawContours(drawFrame, [approx], -1, (0, 200, 50), 3)
-                            angle = np.degrees(np.arctan(float(cx-midw)/float(cy)))
-                            doRobot(angle)
-
-
-                except:
-                    pass
-
-
-            else:
-                stopRobot()
-
-    if blockVisible is True:
-        stopRobot()
+    return cx, cy, conA
 
 cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
 cv2.namedWindow('contours', cv2.WINDOW_NORMAL)
@@ -124,7 +123,7 @@ cv2.resizeWindow('mask', 800, 600);
 cv2.resizeWindow('contours', 800, 600);
 
 #Lower BGR sliders
-cv2.createTrackbar('Bl','mask',76,255,nothing)
+cv2.createTrackbar('Bl','mask',50,255,nothing)
 cv2.createTrackbar('Gl','mask',102,255,nothing)
 cv2.createTrackbar('Rl','mask',90,255,nothing)
 
@@ -133,25 +132,22 @@ cv2.createTrackbar('Bu','mask',95,255,nothing)
 cv2.createTrackbar('Gu','mask',255,255,nothing)
 cv2.createTrackbar('Ru','mask',255,255,nothing)
 
-# resetSliders()
-
 # Reads a single frame in order to determine the height and width of the image
 _, test = cap.read()
 height = test.shape[0]
 width = test.shape[1]
 midw = width/2
 midh = height/2
-print midw,midh
-
-di = 1
-ie = 1
 font = cv2.FONT_HERSHEY_SIMPLEX
+
 robotEnabled = False
+blockVisible = False
 
 while(1):
     _, frame = cap.read()
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    drawFrame = np.copy(frame)
 
     bl = cv2.getTrackbarPos('Bl','mask')
     gl = cv2.getTrackbarPos('Gl','mask')
@@ -165,13 +161,32 @@ while(1):
     upper_BGR = np.array([bu,gu,ru])
     # Threshold the HSV image to get only BGR colors
     mask = cv2.inRange(hsv, lower_BGR, upper_BGR)
+    # print "NON"+str(cv2.countNonZero(mask))
+
+    #
+    angle = None
+    if cv2.countNonZero(mask) > 200: # A fast way of checking for the block before performing further operations on it
+        cx, cy, conA = trackBlock(mask, drawFrame)
+        if cx != 0 and cx != 0 and conA != 0:
+            angle = np.degrees(np.arctan(float(cx-midw)/float(cy)))
 
     cv2.imshow('mask',mask)
 
-    drawFrame = np.copy(frame)
-
-    #Find contours and draw them
-    trackBlock(mask, drawFrame)
+    if blockVisible and angle != None:
+        blockVisible = False
+        print "block is visible!"
+        if robotEnabled:
+            if checkMs(10):
+                doRobot(angle)
+        # seekBlocks()
+    elif not robotEnabled:
+        stopRobot()
+        print "No block found."
+    else:
+        # rotateRobot();
+        print "Block lost!"
+        stopRobot()
+        pass
 
 
     cv2.imshow('contours', drawFrame)
