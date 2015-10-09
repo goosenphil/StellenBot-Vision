4ab84648d0a7fd4609ac2233a8f43f99bb876b07
Julian Gawronsky <juliangawronsky@gmail.com>
2015-10-09 16:03:52 +0200

    Added threading and some further controll functionality and well as minor refactoring

--- tools/blockRush.py
+++ tools/blockRush.py
@@ -5,7 +5,7 @@
 import cv2
 import numpy as np
 import time
-
+import threading
 import serial
 
 try:
@@ -36,39 +36,6 @@ def send(byte1, byte2, byte3):
 def nothing(x):
     pass
 
-tick = 0.0
-def checkMs(diff):
-    newTime = time.time() * 1000
-    global tick
-
-    if (newTime-tick > diff):
-        tick = newTime
-        return True
-
-    return False
-
-stopTick = 0.0
-def checkMsStop(diff):
-    newTimeS = time.time() * 1000
-    global stopTick
-
-    if (newTimeS-stopTick > diff):
-        stopTick = newTimeS
-        return True
-
-    return False
-
-rotTick = 0.0
-def checkMsRotate(diff):
-    newTimeR = time.time() * 1000
-    global rotTick
-
-    if (newTimeR-rotTick  > diff):
-        rotTick  = newTimeR
-        return True
-
-    return False
-
 def drawFeatures(cx, cy, sides, conA):
     cv2.circle(drawFrame,(cx,cy), 5, (255,0,0), -1)
     cv2.putText(drawFrame,(str((cx,cy,conA))),(cx,cy), font, 1,(255,100,50),2,cv2.LINE_AA) # Puts co-ordinates of object
@@ -78,25 +45,54 @@ def drawFeatures(cx, cy, sides, conA):
 # ToDo: make the robot stop less often.
 def doRobot(angle):
     tol = 3
-    speed = (60+ abs(int(angle)))
-    speed = int(speed)
-    if speed > 90:
-        speed = 75
-    if speed < 70:
+    speed = (60+abs(int(angle)))
+    if 70 > speed > 100:
         speed = 75
     # print "[",str(speed),"]"
-    if (tol > abs(angle) ): # Attempt to move forward when on line
+    if ( 7.5 * tol > abs(angle)): # Attempt to move forward when on line
         cv2.putText(drawFrame,"|"+str(speed)+"|",(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
-        send(0, 1, 80)
-        send(1, 1, 80)
-    elif ( angle < -tol): # Attempt to move right towards line
+        send(0, 1, speed+int(angle))
+        send(1, 1, speed-int(angle))
+    elif ( angle < -tol/2): # Attempt to move right towards line
         cv2.putText(drawFrame,"<"+str(speed),(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
         send(1,1,speed)
         send(1,1,speed-10)
-    elif( angle > tol): # Attempt to move left towards line
+    elif( angle > tol/2): # Attempt to move left towards line
         cv2.putText(drawFrame,(str(speed)+">"),(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
         send(0,1,speed)
         send(1,1,speed-10)
+    else:
+        send(0, 1, 80)
+        send(1, 1, 80)
+
+"""
+Do robot alternative
+    offset is the corresponding methode to get the variables
+    
+def doRobot():
+    offset, offset_gradient, visible = getOffset()
+    print offset, offset_gradient, visible
+    error = offset/abs(offset_gradient)
+    gain = error/70
+    tollerance_offset = 30
+
+    threshold_speed = lambda x: x if 60<x<100 else 80
+    speed = 80 #base speed
+    speed_left =  threshold_speed(speed - gain)
+    speed_right = threshold_speed(speed + gain)
+
+    right_bais = 0
+    left_bais = 0
+
+    if(visible): #turns
+        if (tollerance_offset < abs(offset) ): # Attempt to move forward when on line
+            s.write( chr(101) + chr(0)  + chr(1) + chr(int(speed_right)))
+            s.write( chr(101) + chr(1)  + chr(1) + chr(int(speed_left)))
+        else:
+            s.write( chr(101) + chr(0)  + chr(1) + chr(speed))
+            s.write( chr(101) + chr(1)  + chr(1) + chr(speed))
+    else:
+"""
 
 def stopRobot():
     # if checkMsStop(200): # Possibly disable me
@@ -125,9 +121,11 @@ def centroid(contour):
 def seekBlocks():
     rotateRobot()
 
-def trackBlock(mask, drawFrame):
+def trackBlock():
     _, contours, hierarchy = cv2.findContours(np.copy(mask), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     global blockVisible
+    global mask
+    global drawFrame
     blockVisible = False
     contours = sorted(contours, key = cv2.contourArea, reverse = True)[:1] # Reduces contours to only top 5 based on area.
     cx, cy, conA = 0, 0, 0
@@ -174,12 +172,60 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 robotEnabled = False
 blockVisible = False
+angleViable = False
+"""
+def getOffset():
+    cx, cy, conA = trackBlock()
+    if cv2.countNonZero(mask) > 1000:
+        if cx != 0 and cx != 0 and conA != 0:
+             offset_x = average(int(cx-midw))
+             offset_y = int(cy-midh)
+             print "robot ready"
+             offset_gradient = offset_x/offset_y
+             print "no you do robot"
+        return offset_gradient, offset, blockVisible
+    print("failed")
+"""
+
+def getAngle():
+    angle = None
+    angleViable = False
+    if cv2.countNonZero(mask) > 1000: # A fast way of checking for the block before performing further operations on it
+        cx, cy, conA = trackBlock()
+        if cx != 0 and cx != 0 and conA != 0:
+            angle = np.degrees(np.arctan(float(cx-midw)/float(cy)))
+            angleViable = True
+    return angle
+
+"""
+please not we might have to make another global mask for seeking home
+"""
+mask = None
+drawFrame = None
+
+def timer(name, func, delay, cond):
+    print "Timer: " + name + " Started"
+    Lock.acquire()
+    try:
+        print name + " Has Acquired the lock"
+        while cond(): # you can modify the sentinal [key press]
+            time.sleep(delay)
+            func()
+    print name + " is releasing the lock"
+    finally:
+        Lock.release()
+    print "Timer: " + name + " Completed"
+
+t1 = threading.Thread(target=timer, args=("Timer1", doRobot(angle()), 0.02, (blockVisible and angleViable and robotEnabled)))
+t2 = threading.Thread(target=timer, args=("Timer1", robotStop(), 0.02, (not robotEnabled)))
+t3 = threading.Thread(target=timer, args=("Timer1", doRobot(angle()), 0.02, (not blockVisible and robotEnabled)))
+
 
 while(1):
     _, frame = cap.read()
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    drawFrame = np.copy(frame)
+    global drawFrame = np.copy(frame)
 
     bl = cv2.getTrackbarPos('Bl','mask')
     gl = cv2.getTrackbarPos('Gl','mask')
@@ -198,30 +244,25 @@ while(1):
 
     mask = cv2.inRange(hsv, lower_BGR, upper_BGR)
     # print "NON"+str(cv2.countNonZero(mask))
-
-    #
-    angle = None
-    if cv2.countNonZero(mask) > 1000: # A fast way of checking for the block before performing further operations on it
-        cx, cy, conA = trackBlock(mask, drawFrame)
-        if cx != 0 and cx != 0 and conA != 0:
-            angle = np.degrees(np.arctan(float(cx-midw)/float(cy)))
+    getAngle() #updates the states
 
     cv2.imshow('mask',mask)
-
-    if blockVisible and angle != None:
-        blockVisible = False #Possibly use a counter instead to prevent single frame block loss
-        print "block is visible!"
-        if robotEnabled:
-            if checkMs(10):
-                doRobot(angle)
-        # seekBlocks()
-    elif not robotEnabled:
+    try:
+        if(robotEnabled):
+            if(blockVisible()):
+                if(not t1.isAlive() and angleViable):
+                    t1.start()
+            else:
+                if(not t3.isAlive()):
+                    t3.start()
+        else:
+            if(not t2.isAlive()):
+            t2.start()
+    finally:
         stopRobot()
-        print "Robot disabled"
-    else:
-        rotateRobot();
-        print "Block lost! Now rotating robot"
 
+    if(threading.activeCount()<2): #because main is thread
+        robot()
 
     cv2.imshow('contours', drawFrame)
 
