ecf9fd44bc577c3849a1e0ed9414f062c0a6df50
philipgoosen <philip@manmakemachine.com>
2015-10-09 17:37:50 +0200

    removed GUI

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import time
 
 import serial
 
+s = serial.Serial('/dev/ttyACM3', 115200)
+cap = cv2.VideoCapture(1)
+
 try:
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BCM)
@@ -16,22 +19,16 @@ try:
 except ImportError:
     print "[-]Cannot import GPIO interface"
 
-# s = serial.Serial('/dev/pts/21', 115200)
-s = serial.Serial('/dev/ttyACM3', 115200)
 
-cap = cv2.VideoCapture(1)
-
-
-gotBlock = False
 def checkSwitch():
-    return gotBlock
-    # return  !GPIO.input(18)
+    if GPIO.input(18) is False:
+        return True
+    else:
+        return False
 
 # lastSend =
 def send(byte1, byte2, byte3):
     s.write( chr(101) + chr(byte1)  + chr(byte2) + chr(byte3) )
-    # print "SERIAL DISABLED!", str(byte1), str(byte2), str(byte3)
-    pass
 
 def nothing(x):
     pass
@@ -69,16 +66,10 @@ def checkMsRotate(diff):
 
     return False
 
-def drawFeatures(cx, cy, sides, conA):
-    cv2.circle(drawFrame,(cx,cy), 5, (255,0,0), -1)
-    cv2.putText(drawFrame,(str((cx,cy,conA))),(cx,cy), font, 1,(255,100,50),2,cv2.LINE_AA) # Puts co-ordinates of object
-    cv2.putText(drawFrame,(str((midw-cx,height-cy,conA,sides))),(cx,cy+50), font, 1,(155,200,100),2,cv2.LINE_AA) #Distance from bottom centre, contour area and sides
-    cv2.line(drawFrame, (cx,cy),(midw,height), (200,150,0), 5)
-
 # ToDo: make the robot stop less often.
 def doRobot(angle):
     tol = 3
-    speed = (60+ abs(int(angle)))
+    speed = (50+ abs(int(angle)))
     speed = int(speed)
     if speed > 90:
         speed = 75
@@ -86,22 +77,18 @@ def doRobot(angle):
         speed = 75
     # print "[",str(speed),"]"
     if (tol > abs(angle) ): # Attempt to move forward when on line
-        cv2.putText(drawFrame,"|"+str(speed)+"|",(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
         send(0, 1, 80)
         send(1, 1, 80)
     elif ( angle < -tol): # Attempt to move right towards line
-        cv2.putText(drawFrame,"<"+str(speed),(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
         send(1,1,speed)
         send(1,1,speed-10)
     elif( angle > tol): # Attempt to move left towards line
-        cv2.putText(drawFrame,(str(speed)+">"),(100,150), font, 1,(255,255,100),2,cv2.LINE_AA)
         send(0,1,speed)
         send(1,1,speed-10)
 
 def stopRobot():
-    # if checkMsStop(200): # Possibly disable me
-    send(1,1,0)
-    send(0,1,0)
+    s.write( chr(101) + chr(1)  + chr(1) + chr(0) )
+    s.write( chr(101) + chr(0)  + chr(1) + chr(0) )
 
 # Keep on rotating (anti clockswise) if bock does not change in area or co-ordinates enough
 toggleState = False
@@ -125,7 +112,7 @@ def centroid(contour):
 def seekBlocks():
     rotateRobot()
 
-def trackBlock(mask, drawFrame):
+def trackBlock(mask):
     _, contours, hierarchy = cv2.findContours(np.copy(mask), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     global blockVisible
     blockVisible = False
@@ -135,7 +122,7 @@ def trackBlock(mask, drawFrame):
     for c in contours:
         conA = cv2.contourArea(c)
 
-    	if conA > 150:
+    	if conA > 250:
             peri = cv2.arcLength(c, True)
             approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
@@ -144,25 +131,8 @@ def trackBlock(mask, drawFrame):
                 centre = centroid(c)
                 cx = centre[0]
                 cy = centre[1]
-                drawFeatures(cx, cy, len(approx), conA)
-                cv2.drawContours(drawFrame, [approx], -1, (0, 200, 50), 3)
 
     return cx, cy, conA
-
-cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
-cv2.namedWindow('contours', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('mask', 800, 600);
-cv2.resizeWindow('contours', 800, 600);
-
-#Lower BGR sliders
-cv2.createTrackbar('Bl','mask',50,255,nothing)
-cv2.createTrackbar('Gl','mask',102,255,nothing)
-cv2.createTrackbar('Rl','mask',90,255,nothing)
-
-#Upper BGR sliders
-cv2.createTrackbar('Bu','mask',95,255,nothing)
-cv2.createTrackbar('Gu','mask',255,255,nothing)
-cv2.createTrackbar('Ru','mask',255,255,nothing)
 
 # Reads a single frame in order to determine the height and width of the image
 _, test = cap.read()
@@ -172,73 +142,39 @@ midw = width/2
 midh = height/2
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-robotEnabled = False
+robotEnabled = True
 blockVisible = False
+
+lower_BGR = np.array([37, 99, 30])
+upper_BGR = np.array([100,255,255])
 
 while(1):
     _, frame = cap.read()
-    # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    drawFrame = np.copy(frame)
-
-    bl = cv2.getTrackbarPos('Bl','mask')
-    gl = cv2.getTrackbarPos('Gl','mask')
-    rl = cv2.getTrackbarPos('Rl','mask')
-    bu = cv2.getTrackbarPos('Bu','mask')
-    gu = cv2.getTrackbarPos('Gu','mask')
-    ru = cv2.getTrackbarPos('Ru','mask')
-
-    # define range of BGR color in HSV
-    lower_BGR = np.array([bl,gl,rl])
-    upper_BGR = np.array([bu,gu,ru])
     # Threshold the HSV image to get only BGR colors
+    checkSwitch()
     if gotBlock: # Set to beacon if block is found. We can also have a state reset colour?
-        lower_BGR = np.array([0,93,0]) #Current beacon: Yellow screwdriver box
-        upper_BGR = np.array([33,255,255])
+        lower_BGR = np.array([9,150,150]) #Current beacon: Yellow screwdriver box
+        upper_BGR = np.array([160,255,255])
 
     mask = cv2.inRange(hsv, lower_BGR, upper_BGR)
-    # print "NON"+str(cv2.countNonZero(mask))
-
-    #
     angle = None
-    if cv2.countNonZero(mask) > 150: # A fast way of checking for the block before performing further operations on it
-        cx, cy, conA = trackBlock(mask, drawFrame)
+    if cv2.countNonZero(mask) > 200: # A fast way of checking for the block before performing further operations on it
+        cx, cy, conA = trackBlock(mask)
         if cx != 0 and cx != 0 and conA != 0:
             angle = np.degrees(np.arctan(float(cx-midw)/float(cy)))
 
-    cv2.imshow('mask',mask)
 
     if blockVisible and angle != None:
         blockVisible = False #Possibly use a counter instead to prevent single frame block loss
         print "block is visible!"
-        if robotEnabled:
-            if checkMs(10):
-                doRobot(angle)
+        if checkMs(10):
+            doRobot(angle)
         # seekBlocks()
-    elif not robotEnabled:
-        stopRobot()
-        print "Robot disabled"
     else:
         rotateRobot();
         print "Block lost! Now rotating robot"
 
-
-    cv2.imshow('contours', drawFrame)
-
-    k = cv2.waitKey(10) & 0xFF
-    if k == 27: #Checks if escape is pressed
-        break
-    if k == 32: # Press space to toggle robot
-        if robotEnabled == False:
-            robotEnabled = True
-        else:
-            stopRobot()
-            robotEnabled = False
-    if k == 97:
-        print "BLOCK FOUND ACTIVATED. Remember to enable as GPIO"
-        gotBlock = True
-
-cv2.destroyAllWindows()
 s.close()
 
 # What needs to be done:
